159b091baeb54dfbd0f21ab9681e8baff54bb92f
Kozin Artem <kaf-artem@yandex.ru>
2023-04-04 11:43:14 +0300

    Created admin command import users.csv

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,5 +1,6 @@
 from csv import DictReader
 
+from django.conf import settings
 from django.core.management import BaseCommand
 from reviews.models import User
 
@@ -19,5 +20,16 @@ class Command(BaseCommand):
 
         print('Загружаем данные.')
 
-        for row in DictReader(open()):
-            ...
+        for row in DictReader(
+            open(settings.BASE_DIR / 'static' / 'data' / 'users.csv')
+        ):
+            user = User(
+                id=row.get('id'),
+                username=row['username'],
+                email=row['email'],
+                role=row['role'],
+                bio=row['bio'],
+                first_name=row['first_name'],
+                last_name=row['last_name'],
+            )
+            user.save()
